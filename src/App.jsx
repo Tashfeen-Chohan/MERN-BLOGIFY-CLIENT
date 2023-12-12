2571847685423a669f6127b1228aaf03a2c932aa
Tashfeen Chohan <chohantashfeen@gmail.com>
2023-12-13 00:31:48 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./App.css"
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-red-500 font-bold text-5xl text-center'>Tashfeen Chohan</h1>
+    </div>
+  )
+}
+
+export default App

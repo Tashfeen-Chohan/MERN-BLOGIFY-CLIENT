ec00a130b5febef1fd1ae85d7b0c910db6907a69
Tashfeen Chohan <chohantashfeen@gmail.com>
2023-12-19 17:33:38 +0500

    Category header design

--- src/features/category/Categories.jsx
+++ src/features/category/Categories.jsx
@@ -3,32 +3,36 @@ import { useGetCategoriesQuery } from "./categoryApi";
 
 const Categories = () => {
   const {
-    data: allCategoriesData,
+    data: categoires,
     isLoading,
     isError,
     error,
   } = useGetCategoriesQuery();
-  // console.log(allCategoriesData);
 
-  if (isLoading) return <p>Loading....</p>
-  if (isError) return <p>{error}</p>
-
-  console.log("loading: ", isLoading)
-  console.log(allCategoriesData)
+  if (isLoading) return <p>Loading....</p>;
+  if (isError) return <p>{error}</p>;
 
   return (
     <div>
-      <div>
-        <h1>Categories</h1>
-        <div>
-          <input type="text" placeholder="Search any category..." />
-          <button>Search</button>
+      <div className="py-5 flex justify-center items-center flex-col max-w-[90%] mx-auto rounded shadow-lg my-10 bg-slate-100 md:max-w-lg">
+        <h1 className="text-3xl font-bold pb-4 text-center md:pb-7">
+          BLOGIFY APP
+        </h1>
+        <div className="flex justify-center items-center gap-4 w-full px-4 md:pb-2">
+          <input
+            className="shadow-lg border-b-2 px-1 w-[75%] py-[2px] md:w-[50%]  outline-none focus:border-b-2 focus:border-black"
+            type="text"
+            placeholder="Search any category..."
+          />
+          <button className="bg-slate-800 text-white rounded py-1 px-3 shadow-xl hover:bg-slate-700">
+            Search
+          </button>
         </div>
-        <hr />
+      </div>
 
-        <div>
-          <h1>All Categories</h1>
-        </div>
+      <div className="flex justify-center items-center flex-col">
+        <h1 className="text-2xl font-bold ">All Categories</h1>
+        {/* MAIN TABLE */}
       </div>
     </div>
   );

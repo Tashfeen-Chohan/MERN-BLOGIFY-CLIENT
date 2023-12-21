91d59578992d6376d5c16b9921e2717054cdf728
Tashfeen Chohan <chohantashfeen@gmail.com>
2023-12-21 23:37:25 +0500

    category pagination functionality

--- src/features/category/Categories.jsx
+++ src/features/category/Categories.jsx
@@ -3,22 +3,17 @@ import {
   useGetCategoriesQuery,
   useDeleteCategoryMutation,
 } from "./categoryApi";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { AiOutlineSortAscending } from "react-icons/ai";
 
 const Categories = () => {
   const [searchBy, setSearchBy] = useState("");
   const [sortBy, setSortBy] = useState("");
+  const [pageNo, setPageNo] = useState(1);
 
-  let url = `categories?sortBy=${sortBy}&searchBy=${searchBy}`;
-  const {
-    data: categories,
-    isLoading,
-    isError,
-    error,
-  } = useGetCategoriesQuery(url);
+  let url = `categories?sortBy=${sortBy}&searchBy=${searchBy}&page=${pageNo}`;
+  const { data, isLoading, isError, error } = useGetCategoriesQuery(url);
 
   const [deleteCategory] = useDeleteCategoryMutation();
 
@@ -40,13 +35,8 @@ const Categories = () => {
   if (isLoading) return <p>Loading....</p>;
   if (isError) return <p>{error}</p>;
 
-  // const filterCategories = categories.filter((category) => {
-  //   if (searchBy.length === 0By) {
-  //     return category;
-  //   } else {
-  //     return category.name.toLowerCase().includes(searchBy.toLowerCaseBy());
-  //   }
-  // });
+  // OBJECT DESTRUCTURING
+  const { capitalized, totalCategories, totalPages, page, limit } = data;
 
   return (
     <div>
@@ -71,8 +61,8 @@ const Categories = () => {
       <div className="flex justify-center items-center flex-col w-[90%] md:max-w-3xl mx-auto">
         {/* Add Category Container */}
         <div className="flex justify-between items-center w-[90%] md:w-full mb-3 md:mb-0">
-          <span className="bg-slate-800 transition-colors duration-500 hover:bg-slate-700 py-1 px-3 text-white rounded shadow-xl">
-            Total : {categories.length}
+          <span className="bg-slate-800 transition-colors duration-500 hover:bg-slate-700 py-1 px-3 md:px-4 text-white rounded shadow-xl">
+            Total : {totalCategories}
           </span>
           <Link to={`/categories/new`}>
             <button className="py-1 px-3 bg-blue-600 hover:bg-blue-700 transition-colors duration-500 text-white rounded shadow-xl">
@@ -89,9 +79,7 @@ const Categories = () => {
             onChange={(e) => setSortBy(e.target.value)}
             className="bg-slate-200 shadow-md  rounded text-black outline-none px-2 py-1"
           >
-            <option value="">
-              <b>Sort By</b>
-            </option>
+            <option value="">Sort By</option>
             <option value="name">Name Asc</option>
             <option value="name desc">Name Desc</option>
             <option value="date desc">Newest</option>
@@ -116,10 +104,12 @@ const Categories = () => {
               </tr>
             </thead>
             <tbody>
-              {categories.length === 0 ? (
-                  <p className="font-bold text-red-500 my-3 ml-2">No category found!</p>
+              {totalCategories === 0 ? (
+                <p className="font-bold text-red-500 my-3 ml-2">
+                  No category found!
+                </p>
               ) : (
-                categories.map((val, index) => (
+                capitalized.map((val, index) => (
                   <tr
                     key={val._id}
                     className="bg-white border-b hover:bg-gray-50"
@@ -148,20 +138,30 @@ const Categories = () => {
 
         {/* PAGINATION */}
 
-        {/* <div class="flex flex-col items-center mb-5">
-          <span class="text-sm text-gray-700 dark:text-gray-400">
-            Showing{" "}
-            <span class="font-semibold text-gray-900 dark:text-white">1</span>{" "}
-            to{" "}
-            <span class="font-semibold text-gray-900 dark:text-white">10</span>{" "}
-            of{" "}
-            <span class="font-semibold text-gray-900 dark:text-white">100</span>{" "}
+        <div className="flex flex-col items-center mb-8">
+          <span className="text-sm text-gray-700 ">
+            Showing
+            <span className="font-semibold text-gray-900 px-1">
+              {page * limit - (limit - 1)}
+            </span>
+            to
+            <span className="font-semibold text-gray-900 px-1">
+              {page * limit > totalCategories ? totalCategories : page * limit}
+            </span>
+            of
+            <span className="font-semibold text-gray-900 px-1">
+              {totalCategories}
+            </span>
             Entries
           </span>
-          <div class="inline-flex mt-2 xs:mt-0">
-            <button class="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-gray-800 rounded-s hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+          <div className="inline-flex mt-2 xs:mt-0">
+            <button
+              disabled={page === 1}
+              onClick={() => setPageNo(page - 1)}
+              className="flex items-center justify-center px-3 h-8 text-sm font-medium text-black bg-slate-200  hover:bg-slate-300 transition-colors duration-300"
+            >
               <svg
-                class="w-3.5 h-3.5 me-2 rtl:rotate-180"
+                className="w-3.5 h-3.5 me-2 rtl:rotate-180"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -169,18 +169,22 @@ const Categories = () => {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M13 5H1m0 0 4 4M1 5l4-4"
                 />
               </svg>
               Prev
             </button>
-            <button class="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-gray-800 border-0 border-s border-gray-700 rounded-e hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+            <button
+              disabled={page === totalPages}
+              onClick={() => setPageNo(page + 1)}
+              className="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-slate-700 border-0 border-s  rounded-e hover:bg-slate-900 transition-colors duration-300"
+            >
               Next
               <svg
-                class="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+                className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -188,15 +192,15 @@ const Categories = () => {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M1 5h12m0 0L9 1m4 4L9 9"
                 />
               </svg>
             </button>
           </div>
-        </div> */}
+        </div>
       </div>
     </div>
   );

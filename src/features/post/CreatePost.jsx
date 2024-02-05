c5c590655337073eb892b83c59124f83aa1abf6d
Tashfeen Chohan <chohantashfeen@gmail.com>
2024-02-06 01:33:06 +0500

    created postApi using rtk query

--- src/features/post/CreatePost.jsx
+++ src/features/post/CreatePost.jsx
@@ -1,16 +1,39 @@
 import React, { useState } from "react";
 import { useGetCategoriesQuery } from "../category/categoryApi";
 import Select from "react-select";
+import { toast } from "react-toastify";
 
 const CreatePost = () => {
-  const [selectedOption, setSelectedOption] = useState(null);
   const url = `categories?sortBy=name&limit=1000`;
   const { data } = useGetCategoriesQuery(url);
 
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [cover, setCover] = useState("");
+
+
   const options = data?.capitalized.map((val) => ({
     value: val.name,
-    label: val.name
+    label: val.name,
   }));
+
+  const handleCategoryChange = (selectedOptions) => {
+    setSelectedCategories(selectedOptions || []);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (title, desc, selectedCategories.length > 0){
+      try {
+        
+      } catch (error) {
+        
+      }
+    } else {
+      toast.error("Please provide neccessary details!")
+    }
+  }
 
   return (
     <div className="flex justify-center items-center min-h-screen mt-[-60px]">
@@ -18,13 +41,15 @@ const CreatePost = () => {
         <h1 className="text-center text-2xl md:text-3xl  font-bold pb-7">
           Create Post
         </h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           {/* TITLE */}
           <div className="mb-3">
             <input
               className="w-full py-2 px-3 rounded text-xl outline-none"
               type="text"
               placeholder="Title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
             />
           </div>
           {/* DESCRIPTION */}
@@ -32,15 +57,16 @@ const CreatePost = () => {
             <textarea
               className="h-52 w-full py-2 px-3 rounded text-lg outline-none"
               placeholder="Post description"
+              value={desc}
+              onChange={(e) => setDesc(e.target.value)}
             ></textarea>
           </div>
           {/* CATEGORY */}
           <div className="flex gap-2 md:gap-5 justify-start  items-center flex-col md:flex-row">
-           
             <Select
-            className="w-full md:w-[50%]"
-              defaultValue={selectedOption}
-              onChange={setSelectedOption}
+              className="w-full md:w-[50%]"
+              defaultValue={selectedCategories}
+              onChange={handleCategoryChange}
               options={options}
               isMulti
               isSearchable
@@ -51,8 +77,8 @@ const CreatePost = () => {
           </div>
           {/* SUBMIT */}
           <div className="md:mt-3 mt-4 flex justify-end items-center gap-3">
-            <button className="bg-indigo-600 hover:bg-indigo-700 transition-colors duration-300 px-5 py-1 text-white rounded shadow-xl">
-              Post
+            <button type="submit" className="bg-indigo-600 hover:bg-indigo-700 transition-colors duration-300 px-5 py-1 text-white rounded shadow-xl">
+              Publish
             </button>
             <button className="bg-rose-500 hover:bg-rose-600 transition-colors duration-300 px-3 py-1 text-white rounded shadow-xl">
               Cancel
---
+++ src/features/post/postApi.jsx
@@ -0,0 +1,55 @@
+import { apiSlice } from "../../app/apiSlice";
+
+export const postApi = apiSlice.injectEndpoints({
+  
+  endpoints: (builder) => ({
+    // GET ALL POSTS
+    getPosts: builder.query({
+      query: (url) => url,
+      providesTags: ["Post"]
+    }),
+
+    // GET SINGLE POST
+    getSinglePost: builder.query({
+      query: (id) => `/posts/${id}`,
+      providesTags: ["Post"]
+    }),
+
+    // CREATE NEW POST
+    createPost: builder.mutation({
+      query: (newPost) => ({
+        url: "/posts",
+        method: "POST",
+        body: newPost
+      }),
+      invalidatesTags: ["Post"]
+    }),
+
+    // UPDATE POST
+    updatePost: builder.mutation({
+      query: ({id, ...updatedPost}) => ({
+        url: `/posts/${id}`,
+        method: "PATCH",
+        body: updatedPost
+      }),
+      invalidatesTags: ["Post"]
+    }),
+
+    // DELETE POST
+    deletePost: builder.mutation({
+      query: (id) => ({
+        url: `/posts/${id}`,
+        method: "DELETE"
+      }),
+      invalidatesTags: ["Post"]
+    })
+  })
+})
+
+export const {
+  useGetPostsQuery ,
+  useGetSinglePostQuery,
+  useCreatePostMutation,
+  useUpdatePostMutation,
+  useDeletePostMutation
+} = postApi;

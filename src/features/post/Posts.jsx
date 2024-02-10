9064b2bde7fd47a645b50a52e575b44ea812c594
Tashfeen Chohan <chohantashfeen@gmail.com>
2024-02-10 15:09:18 +0500

    See More pagination

--- src/features/post/Posts.jsx
+++ src/features/post/Posts.jsx
@@ -6,7 +6,8 @@ import { MdSearch } from "react-icons/md";
 
 const Posts = () => {
   const [search, setSearch] = useState("")
-  const postUrl = `/posts?searchBy=${search}`
+  const [limit, setLimit] = useState(6)
+  const postUrl = `/posts?searchBy=${search}&limit=${limit}`
   const { data, isLoading } = useGetPostsQuery(postUrl);
   const navigate = useNavigate();
 
@@ -75,17 +76,17 @@ const Posts = () => {
     <div className="flex justify-center items-center">
       <div >
         {/* HEADER */}
-        <div className="my-10 flex justify-center items-center flex-col">
+        <div className="my-10 p-3 flex justify-center items-center flex-col mx-auto rounded-lg w-[95%] md:max-w-xl shadow-xl">
           <Link className="text-sm" to={"/register"}>Get started with <span className="font-bold italic">Blogify</span></Link>
           <h2 className="text-2xl md:text-3xl font-bold">What are you looking up-to?</h2>
-          <div className="flex justify-center items-center gap-2 mt-2 rounded-lg shadow-xl py-2 px-3">
-            <MdSearch color="gray" size={20}/>
-            <input  className="py-1 px-2 outline-none" type="text" placeholder="Search any blog..." onChange={(e) => setSearch(e.target.value)} />
+          <div className="flex md:w-full justify-center items-center gap-3 mt-2 rounded-lg py-2 px-3">
+            <MdSearch color="gray" size={22}/>
+            <input  className="md:w-[60%] py-1 px-2 outline-none border-b-2 focus-within:border-black" type="text" placeholder="Search any blog..." onChange={(e) => setSearch(e.target.value)} />
             <button className="bg-slate-700 hover:bg-slate-800 transition-colors duration-300 text-white rounded py-1 px-3">Search</button>
           </div>
         </div>
         {/* POSTS */}
-        <div className="grid mx-auto grid-cols-12 gap-5 w-[95%] md:max-w-5xl my-7">
+        <div className="grid mx-auto grid-cols-12 gap-7 md:gap-x-5 md:gap-y-7 w-[95%] md:max-w-5xl my-7">
           {Posts}
         </div>
         {/* NO POST FOUND */}
@@ -98,6 +99,10 @@ const Posts = () => {
         >
           <p>No post found!</p>
         </div>
+        {/* PAGINATION */}
+        {(totalPosts !== 0 && limit < totalPosts)  &&  <div className="my-5 flex justify-center items-center ">
+          <button onClick={() => setLimit(limit + 6)} className="py-1 px-3 rounded shadow-xl bg-slate-700 text-white hover:bg-slate-800 transition-colors duration-300" >See More</button>
+        </div>}
       </div>
     </div>
   );

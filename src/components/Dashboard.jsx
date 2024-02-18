da507a939a61b6b3759d58cf94082feb37ce9bf6
Tashfeen Chohan <chohantashfeen@gmail.com>
2024-02-19 01:38:48 +0500

    Dashboard - recent comments

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useGetUsersQuery } from "../features/user/userApi";
 import { TbUsersGroup } from "react-icons/tb";
 import { RiArticleLine } from "react-icons/ri";
@@ -20,31 +20,33 @@ import { FaRegComment, FaRegEye } from "react-icons/fa";
 const Dashboard = () => {
   const { data: Users, isLoading } = useGetUsersQuery("/users");
   const { data: Posts, isLoading: postLoading } = useGetPostsQuery("/posts");
-  const { data: Popular, isLoading: popularLoading } = useGetPostsQuery(
-    "/posts?filterBy=popular"
-  );
   const { data: Comments, isLoading: comLoading } =
     useGetAllCommentsQuery("/comments");
   const { data: Categories, isLoading: catLoading } =
     useGetCategoriesQuery("/categories");
   const { data, isLoading: totalLoading } =
     useGetTotalLikesAndViewsQuery("/posts/likes-views");
+  const { data: allComments, allComLoading } =
+    useGetAllCommentsQuery("/comments");
+
+  const [limit, setLimit] = useState(3);
+  const [sort, setSort] = useState("");
+  let url = `posts?filterBy=popular&sortBy=${sort}&limit=${limit}`;
+  const { data: Popular, isLoading: popularLoading } = useGetPostsQuery(url);
 
   const navigate = useNavigate();
 
-  if (
-    isLoading ||
-    postLoading ||
-    comLoading ||
-    catLoading ||
-    totalLoading ||
-    popularLoading
-  )
+  if (isLoading || postLoading || comLoading || catLoading || totalLoading)
     return (
       <div className="flex justify-center items-center min-h-screen mt-[-60px] bg-slate-200">
         <BeatLoader color="#000000" size={15} />
       </div>
     );
+
+  const viewPost = (id) => {
+    navigate(`/posts/single/${id}`);
+    window.scrollTo(0, 0);
+  };
 
   return (
     <div>
@@ -103,10 +105,7 @@ const Dashboard = () => {
           </div>
         </div>
         {/* LIKES */}
-        <div
-          onClick={() => navigate("/categories")}
-          className="hover:scale-110 hover:bg-indigo-600 bg-indigo-500 text-gray-100 transition-all duration-300 col-span-6 md:col-span-4 py-4 md:py-5 shadow-xl rounded flex justify-between md:justify-center md:gap-5 px-3 items-center "
-        >
+        <div className="hover:scale-110 hover:bg-indigo-600 bg-indigo-500 text-gray-100 transition-all duration-300 col-span-6 md:col-span-4 py-4 md:py-5 shadow-xl rounded flex justify-between md:justify-center md:gap-5 px-3 items-center ">
           <PiHandsClappingFill size={50} />
           <div className="flex justify-center items-center flex-col">
             <p className="text-xl font-bold">{data?.totalLikes}</p>
@@ -115,55 +114,134 @@ const Dashboard = () => {
         </div>
       </div>
 
-      {/* TOP ARTICLE */}
-      <div className="w-[95%] mx-auto my-7 max-w-lg">
-        {/* HEADING & SORTING */}
-        <div className="flex justify-between items-center">
-          <h2 className="text-lg font-bold">Top Articles</h2>
-          <select className="outline-none py-1 px-3 border-b-2 shadow-sm">
-            <option value="">Recent</option>
-            <option value="">Oldest</option>
-          </select>
+      {/* TOP ARTICLE & RECENT COMMENT*/}
+      {popularLoading ? (
+        <div className="w-[95%] flex justify-center items-center my-7">
+          <BeatLoader color="#000000" size={15} />
         </div>
-        {/* ARTICLES */}
-        <div>
-          {Popular?.posts.map((post) => (
-            <div className="mt-4 shadow px-1 md:px-5" key={post._id}>
-              <div className="flex justify-start items-start  gap-3">
-                <img
-                  src={post.blogImg}
-                  className="w-16 h-16 md:h-24 md:w-24 rounded object-cover"
-                  alt=""
-                />
-
-                <div>
-                  <h2 className="font-bold line-clamp-2">{post.title}</h2>
-                  <div>
-                    <span>{moment(post.createdAt).fromNow()}</span>
-                    <span className="mx-3"> - </span>
-                    <span>{post.author.username}</span>
-                  </div>
-                  <div className="flex justify-start items-center gap-3 mt-1.5">
-                    <div className="flex justify-start items-center gap-1">
-                      <PiHandsClappingLight />
-                      <span className="text-sm">{post.likes}</span>
-                    </div>
-                    <div className="flex justify-start items-center gap-1">
-                      <FaRegComment />
-                      <span className="text-sm">{post.commentsCount}</span>
-                    </div>
-                    <div className="flex justify-start items-center gap-1">
-                      <FaRegEye />
-                      <span className="text-sm">{post.views}</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <hr className="mt-2 shadow-xl"/>
+      ) : (
+        <div className="w-[95%] flex flex-col md:flex-row mx-auto my-7 md:my-10 md:max-w-4xl gap-7">
+          {/* TOP ARTICLE */}
+          <div className="md:w-[60%]">
+            {/* HEADING & SORTING */}
+            <div className="flex justify-between items-center">
+              <h2 className="text-lg font-bold font-serif">Top Articles</h2>
+              <select
+                onChange={(e) => setSort(e.target.value)}
+                className="outline-none py-1 px-3 border-b-2 shadow-sm"
+              >
+                <option value="">Recent</option>
+                <option value="oldest">Oldest</option>
+              </select>
             </div>
-          ))}
+            {/* ARTICLES */}
+            <div>
+              {Popular?.posts.map((post) => (
+                <div
+                  className="hover:bg-gray-100 pt-3 mt-4 shadow px-1 md:px-5"
+                  key={post._id}
+                >
+                  <div className="flex justify-start items-start  gap-3">
+                    <img
+                      onClick={() => viewPost(post._id)}
+                      src={post.blogImg}
+                      className="cursor-pointer w-16 h-16 md:h-24 md:w-24 rounded object-cover"
+                      alt=""
+                    />
+                    {/* POST INFO */}
+                    <div>
+                      <h2
+                        onClick={() => viewPost(post._id)}
+                        className="cursor-pointer font-bold line-clamp-2"
+                      >
+                        {post.title}
+                      </h2>
+                      <div>
+                        <span>{moment(post.createdAt).fromNow()}</span>
+                        <span className="mx-3"> - </span>
+                        <span
+                          className="cursor-pointer"
+                          onClick={() =>
+                            navigate(`/users/single/${post.author._id}`)
+                          }
+                        >
+                          {post.author.username}
+                        </span>
+                      </div>
+                      <div className="flex justify-start items-center gap-3 mt-1.5">
+                        <div className="flex justify-start items-center gap-1">
+                          <PiHandsClappingLight />
+                          <span className="text-sm">{post.likes}</span>
+                        </div>
+                        <div className="flex justify-start items-center gap-1">
+                          <FaRegComment />
+                          <span className="text-sm">{post.commentsCount}</span>
+                        </div>
+                        <div className="flex justify-start items-center gap-1">
+                          <FaRegEye />
+                          <span className="text-sm">{post.views}</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <hr className="mt-2 shadow-xl" />
+                </div>
+              ))}
+              {/* SEE MORE BUTTON */}
+              {limit < Popular?.totalPosts && (
+                <div
+                  onClick={() => setLimit(limit + 3)}
+                  className="flex justify-center items-center mt-3"
+                >
+                  <button className="hover:bg-cyan-400 transition-colors duration-300 bg-cyan-500 text-white py-1 px-3 rounded shadow-xl">
+                    See More
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+          {/* RECENT COMMENTS */}
+          <div className="md:w-[50%]">
+            {/* HEADING */}
+            <div className="flex justify-between items-center">
+              <h2 className="text-lg font-bold font-serif">Recent Comments</h2>
+              <button className="bg-cyan-500 text-white hover:bg-cyan-400 transition-colors duration-300 py-1 px-3 rounded shadow-xl">
+                See All
+              </button>
+            </div>
+            {/* COMMENTS */}
+            <div className="relative overflow-x-auto md:w-full shadow-md sm:rounded mb-5 mt-3 w-[100%]">
+              <table className="w-full text-sm text-left rtl:text-right">
+                <thead className="text-md text-black uppercase bg-slate-300">
+                  <tr>
+                    <th scope="col" className="px-6 py-3">
+                      CONTENT
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      LIKES
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="font-semibold! text-sm">
+                  {allComments?.map((val, index) => (
+                    <tr
+                      key={val._id}
+                      className="userTable bg-white border-b hover:bg-gray-200"
+                    >
+                      <td className="px-6 py-2">
+                          {val.content}
+                      </td>
+                      <td className="px-6 py-2 text-right flex justify-start items-center gap-2">
+                        {val.likes}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
